95209e4563330dc22df59bfe5301fe354f544deb
Jenrik Lücker <159541473+TwombiE@users.noreply.github.com>
2025-11-01 20:51:44 +0100

    Add files via upload

---
+++ js/apps.js
@@ -0,0 +1,23 @@
+export const sections = {
+    weather: `
+        <div id="weather-map">
+            <p>Lade Wetterdaten...</p>
+        </div>
+    `,
+    calendar: `
+        <h2>Profile</h2>
+        <p>Hier ist dein Profil.</p>
+    `,
+    focusfabric: `
+        <h2>Messages</h2>
+        <p>Hier sind deine Nachrichten.</p>
+    `,
+    qr: `
+        <h2>Photos</h2>
+        <p>Hier sind deine Fotos.</p>
+    `,
+    settings: `
+        <h2>Settings</h2>
+        <p>Hier kannst du die Einstellungen ändern.</p>
+    `
+};
